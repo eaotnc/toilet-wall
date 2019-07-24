3e5aee67f031e21108442152dc99a237ab9563b6
Thanatcha <bonsai.eao@gmail.com>
2019-07-25 02:58:19 +0700

    complete css style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import CanvasDraw from "react-canvas-draw";
-
+import { Button } from 'antd';
+import './App.css'
+import 'antd/dist/antd.css'; 
 class App extends Component {
   state = {
     color: "#ffc600",
-    width: 400,
-    height: 400,
+    width: 320,
+    height: 320,
     brushRadius: 3,
     lazyRadius: 3,
     savedPictureList:[],
@@ -16,44 +18,29 @@ class App extends Component {
       this.setState({
         color: "#" + Math.floor(Math.random() * 16777215).toString(16)
       });
-    }, 1000);
+    }, 1500);
   }
   handleSavePicture=()=>{
-    
-      
+    const data =JSON.parse(this.saveableCanvas.getSaveData())
+    console.log('this.saveableCanvas.getSaveData()', data.lines)
+    if(data.lines.length>0){
       const savedPictureList= [...this.state.savedPictureList,this.saveableCanvas.getSaveData() ]
       this.setState({
         savedPictureList
       })
+    }
   
   }
   render() {
     const {savedPictureList}=this.state
     return (
-      <div>
-        <div>
-          <button
-            onClick={this.handleSavePicture}
-          >
-            Save
-          </button>
-          <button
-            onClick={() => {
-              this.saveableCanvas.clear();
-            }}
-          >
-            Clear
-          </button>
-          <button
-            onClick={() => {
-              this.saveableCanvas.undo();
-            }}
-          >
-            Undo
-          </button>
-        </div>
-
+      <div className="App">
+        <div className="canvas-drower">
+          <div className="label">
+            Eao toilet Wall
+          </div>
         <CanvasDraw
+          style={{background: "#6b6b6b47" ,zIndex:"25",borderRadius:'14px'}}
           hideGrid
           ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
           brushColor={this.state.color}
@@ -62,18 +49,25 @@ class App extends Component {
           canvasWidth={this.state.width}
           canvasHeight={this.state.height}
         />
-     
-        <button
-          onClick={() => {}}
-        >
-       
-        </button>
+          <div>
+          <Button onClick={this.handleSavePicture} > Save  </Button>
+          <Button onClick={() => {this.saveableCanvas.clear()}} >
+            Clear
+          </Button>
+          <Button onClick={() => {this.saveableCanvas.undo()}}  >
+            Undo
+          </Button>
+        </div>
+        </div>
 
         { savedPictureList.length>0?
           savedPictureList.map((savedPicture)=>
          <CanvasDraw
+          style={{background: "#232323"}}
           disabled
-          hideGrid
+          hideGrid 
+          canvasWidth={this.state.width}
+          canvasHeight={this.state.height}
           ref={canvasDraw => (this.loadableCanvas = canvasDraw)}
           saveData={savedPicture}
         />)
